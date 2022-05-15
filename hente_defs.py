152b60f28a9986b0e31a2c86b9a252c9bb084369
carlos reiakvam <cre032@uit.no>
2022-05-15 15:25:17 +0200

    new

--- hente_defs.py
+++ hente_defs.py
@@ -1,16 +1,23 @@
 filename = r"C:\Users\sogsquid\Desktop\midlertidig_plassering\munchvrak\backend\auth\queries.py"
-file = open(filename)
 
 
 def get_defs():
-    print("\n\nDEFS\n\n")
+    file = open(filename)
+    print("\n\nMETODER\n\n")
     for line in file:
         if line[0:3] == 'def':
             print(line[0:len(line) - 2])
+    file.close()
 
 
 def get_classes():
-    print("\n\nDEFS\n\n")
+    file = open(filename)
+    print("\n\nKLASSER\n")
+    for line in file:
+        if line[0:4] == 'class':
+            print(line[0:len(line) - 2])
+    file.close()
 
 
 get_defs()
+get_classes()
