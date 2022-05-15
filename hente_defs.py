700d30ce37be4d9ff3cc0c142a6ae50123decaa1
carlos reiakvam <cre032@uit.no>
2022-05-15 15:20:15 +0200

    new

--- hente_defs.py
+++ hente_defs.py
@@ -1,10 +1,12 @@
 filename = r"C:\Users\sogsquid\Desktop\midlertidig_plassering\munchvrak\backend\auth\queries.py"
 file = open(filename)
 
-defs = []
-for line in file:
-    if line[0:3] == 'def':
-        print(line[0:len(line) - 2])
 
-print(*defs)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def get_defs():
+    defs = []
+    for line in file:
+        if line[0:3] == 'def':
+            print(line[0:len(line) - 2])
+    print(*defs)
+
+get_defs()
